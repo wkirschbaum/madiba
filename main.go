b6c81137466501b15186556ae7a6e48fe039471f
Wilhelm Kirschbaum <wkirschbaum@gmail.com>
2016-07-11 20:19:02 +0200

    Initial loop

---
+++ main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/nsf/termbox-go"
+)
+
+var current string
+var curev termbox.Event
+
+func redraw() {
+
+}
+
+func main() {
+	err := termbox.Init()
+	if err != nil {
+		panic(err)
+	}
+	defer termbox.Close()
+	termbox.SetInputMode(termbox.InputAlt | termbox.InputMouse)
+	redraw()
+
+	data := make([]byte, 0, 64)
+mainloop:
+	for {
+		if cap(data)-len(data) < 32 {
+			newdata := make([]byte, len(data), len(data)+32)
+			copy(newdata, data)
+			data = newdata
+		}
+		beg := len(data)
+		d := data[beg : beg+32]
+		switch ev := termbox.PollRawEvent(d); ev.Type {
+		case termbox.EventRaw:
+			data = data[:beg+ev.N]
+			current = fmt.Sprintf("%q", data)
+			if current == `"q"` {
+				break mainloop
+			}
+
+			for {
+				ev := termbox.ParseEvent(data)
+				if ev.N == 0 {
+					break
+				}
+				curev = ev
+				copy(data, data[curev.N:])
+				data = data[:len(data)-curev.N]
+			}
+		case termbox.EventError:
+			panic(ev.Err)
+		}
+		redraw()
+	}
+}
